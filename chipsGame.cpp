c1e1009791d5dee1f93c88fcde238f663a8ec012
Dhan V Sagar <dhanvsagar@gmail.com>
2018-10-07 18:30:06 +0530

    Update chipsGame.cpp
    Added writing result to file

--- chipsGame.cpp
+++ chipsGame.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
 string FindPlayerName(string names[], bool playerTurn);
 void GetUserName(string players[]);
 int AskMove(bool player1Turn, int chipsInPile, string names[]);
+void WriteWinner(string name, int movesCount);
 
 const int MAX_CHIPS = 100;
 const float MAX_TURN = 0.5;
+const string OUT_FILE_NAME = "Winners and Moves.txt";
 
 int main()
 {
@@ -17,7 +20,9 @@ int main()
   int chipsInPile = 0;
   int chipsTaken  = 0;
   int maxPerTurn = 0;
+  int noOfMoves = 0;
   char nextGame = 'N';
+  string winnerName = "";
   string playerName[2] = {"Tim", "Tom"};
   
   //seed the random number generator
@@ -32,7 +37,8 @@ int main()
     bool gameOver = false;
     chipsInPile = (rand() % MAX_CHIPS) + 1;
     cout << "Game started with " << chipsInPile << " chips \n" << endl;
-    
+    noOfMoves = 0;
+
     while( gameOver == false)
     {
       chipsTaken = AskMove(player1Turn, chipsInPile, playerName);
@@ -42,13 +48,16 @@ int main()
       if (chipsInPile == 0)
       {
         gameOver = true;
-        cout << FindPlayerName(playerName, !player1Turn)  << ", You Won..!!! " <<endl;
+        winnerName = FindPlayerName(playerName, !player1Turn);
+        cout << winnerName << ", You Won..!!! " <<endl;
       }
       else
       {
         player1Turn = !player1Turn;
+        noOfMoves++;
       }
     }
+    WriteWinner(winnerName, noOfMoves);
     cout << "Do you want to start a new game...? " << endl;
     cout << "Press Y or N :\t";
     cin >> nextGame;
@@ -99,4 +108,19 @@ int AskMove(bool player1Turn, int chipsInPile, string names[])
         //} while (chipsTaken > static_cast<int>(chipsInPile * MAX_TURN));
       } while ( (chipsTaken > maxPerTurn) && (chipsInPile > 1) );
       return chipsTaken;
+}
+
+void WriteWinner(string name, int movesCount)
+{
+  ofstream outFile;
+  outFile.open(OUT_FILE_NAME, ios::app);
+  if (!outFile.fail())
+  {
+    outFile << "Winner : "<< name << " Moves : " << movesCount << endl;
+  }
+  else
+  {
+    cout << "Error writting the Results " ;
+  }
+  outFile.close();
 }
